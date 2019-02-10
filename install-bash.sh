fc25ae0c01617bc9dedd2571d6f11fd06e6b2122
Matt Riley <spam@oscmp.com>
2019-02-10 15:29:13 -0500

    create install script

---
+++ install-bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/rileymat/config-files/master/bash/.bash_profile > ~/.bash_profile
+curl https://raw.githubusercontent.com/rileymat/config-files/master/bash/.bashrc > ~/.bashrc
+curl https://raw.githubusercontent.com/rileymat/config-files/master/bash/.bashrc_mac > ~/.bashrc_mac
+curl https://raw.githubusercontent.com/rileymat/config-files/master/bash/.bash_aliases > ~/.bash_aliases
+curl https://raw.githubusercontent.com/rileymat/config-files/master/bash/.bash_aliases_mac > ~/.bash_aliases_mac
