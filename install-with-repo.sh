e2660547f01928fc1c4b9fa1ae757ef490ebe260
Matt Riley <mattriley@oscmp.com>
2023-11-20 09:01:14 -0500

    Add make-symlinks.sh

---
+++ install-with-repo.sh
@@ -0,0 +1,3 @@
+git clone git@github.com:rileymat/config-files.git ~/.config-files-repo
+cd ~/.git-files-repo
+./make-sym-links.sh
--- make-sym-links.sh
+++ make-sym-links.sh
@@ -1,2 +1,32 @@
-ln -s "$(readlink -f ./emacs/.emacs)" ~/.emacs
-ln -s "$(readlink -f ./emacs/.emacs.d)" ~/.emacs.d
+mkdir ~/.makesymback
+
+
+backupAndLink(){
+	mv ~/$1  ~/.makesymback/$1
+	ln -s "$(readlink -f ./$2/$1)" ~/$1
+}
+
+backupAndLink ".emacs"  "emacs"
+backupAndLink ".emacs.d" "emacs"
+backupAndLink ".bash_profile" "bash"
+backupAndLink ".bashrc" "bash"
+backupAndLink ".bashrc_mac" "bash"
+backupAndLink ".bash_aliases" "bash"
+backupAndLink ".bash_aliases_mac" "bash"
+
+
+#mv ~/.emacs ~/.makesymback/.emacs
+#ln -s "$(readlink -f ./emacs/.emacs)" ~/.emacs
+
+#mv ~/.emacs.d ~/.makesymback/.emacs.d
+#ln -s "$(readlink -f ./emacs/.emacs.d)" ~/.emacs.d
+
+#ln -s "$(readlink -f ./bash/.bash_profile)"  ~/.bash_profile
+
+#ln -s "$(readlink -f ./bash/.bashrc)"  ~/.bashrc
+
+#ln -s "$(readlink -f ./bash/.bashrc_mac)"  ~/.bashrc_mac
+
+#ln -s "$(readlink -f ./bash/.bash_aliases)"  ~/.bash_aliases
+
+#ln -s "$(readlink -f ./bash/.bash_aliases_mac)"  ~/.bash_aliases_mac
